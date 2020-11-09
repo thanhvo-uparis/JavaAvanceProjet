d019cc4329036a8ac5727897a8132c8d3f2ec987
yanntrividic <yann.trividic@hotmail.fr>
2020-11-09 19:13:05 +0100

    Réflexions sur la structure du programme et de ses packages

---
+++ src/outils/Tester.java
@@ -0,0 +1,28 @@
+package outils;
+import constructionEcoles.Agglomeration; 
+
+public class Tester {
+	// cette classe va contenir les méthodes qui permettront de tester les algos. Il faut y mettre le contenu de la classe Test
+	
+	public static Agglomeration AggloGenerateurRandomOperations(int nbVilles, int nbOperations) {
+		//classe Test à mettre ici
+		
+		//temporaire juste pour qu'il n'y ait pas d'erreurs 
+		return new Agglomeration() ;
+	}
+	
+	
+	public static Agglomeration RandomAggloGenerateur(int nbVilles) {
+	//classe classe qui renvoie une Agglomération aléatoire
+	
+	//temporaire juste pour qu'il n'y ait pas d'erreurs 
+		return new Agglomeration() ;
+	}
+	
+	//pour tester la complexité d'un algo en particulier
+	public static double testComplexiteAlgo(Agglomeration agg) {
+		return 0 ;
+	}
+	
+	// etc etc.
+}

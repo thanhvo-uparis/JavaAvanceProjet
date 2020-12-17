52a3153f9f7d337c6d6d468de45f9b899e8c0693
anthony-baptista <baptista.anthony@yahoo.fr>
2020-12-17 02:00:54 +0100

    Reglage du probleme de variable (nouveau prob survenu autrepart) ajout des commentaires sur la classe menuInitial Mise a jour des menus retrait de la contrainte des 26 villes à la création de l'agglo modifs mineurs

--- src/main/menus/MenuInitial.java
+++ src/main/menus/MenuInitial.java
@@ -12,43 +12,46 @@ public class MenuInitial {
 	
 	public static void lancement() {
 		MenuInitial.menuCreationAgglo();
-		MenuInitial.menuPrincipal(); //
+		MenuInitial.menuPrincipal(agg); //PROBLEME DE VARIABLE ENCORE MAL DECLAR2 JE SUPPOSE A APPARU DEPUIS QUE J4AI REGLE LES AUTRES PROBLEMES DE LA VARIABLE AGG
 	}
 		
-	public static Agglomeration menuCreationAgglo() {
+	public static void menuCreationAgglo() {
 		// Charger une agglomération
 				//		a) charger manuellement
-				//		b) charger é partir d'un fichier (LectureEcriture.lectureDepuisFichier("string") ;)
+				//		b) charger à partir d'un fichier (LectureEcriture.lectureDepuisFichier("string") ;)
 		Scanner sc = new Scanner(System.in);
 		int choice = 0; 
 		boolean exit ;
 
 		do {
-			System.out.println("\n  - Charger une agglomération - ") ;
+			System.out.println("\n  - Charger une agglomàration - ") ;
 			System.out.println("1 - Charger manuellement");
 			System.out.println("2 - Charger depuis un fichier");
 			System.out.print("Veuillez entrer votre choix : ");
 
 			switch (choice) {
 			case 1 :
+				//Charge une agglomàration de facon manuelle grâce à la fonction aggloManuelle()
 				aggloManuelle();
 				sc.close();
 				break ;
 			case 2 :
-				System.out.println("Rentrez le chemin vers le fichier nécessaire é la création de l'agglomération");
+				//Charge l'agglomàration depuis un fichier grâce à la fonction LectureDepuisFichier de la classe LectureEcriture
+				System.out.println("Rentrez le chemin vers le fichier nécessaire à la cràation de l'agglomération");
 				LectureEcriture.lectureDepuisFichier(sc.nextLine()) ;
 				sc.close();
 				break ;
 			default :
+				//affiche choix incorrect si le choix rentrà n'est pas 1 ou 2 
 				System.out.println("choix incorrect") ;
 				break ;
 			}
 			exit = (choice == 1) || (choice == 2);		
 		} while (!exit);
-		return agg;
+		
 	}
-	
-	private static void menuPrincipal() {
+	//Crée le menuPrincipal qui prend comme argument l'agglomération agg et qui nous propose 4 choix
+	private void menuPrincipal(Agglomeration agg) {//LIé AU PROBLEME DE VARIABLE LIGNE 15 
 		Scanner sc = new Scanner(System.in);
 		int choice=0;
 		boolean exit;
@@ -57,54 +60,52 @@ public class MenuInitial {
 			System.out.println("1 - Résoudre manuellement");
 			System.out.println("2 - Résoudre automatiquement");
 			System.out.println("3 - sauvegarder") ;
-			System.out.println("4 - tester la complexité des algorithmes") ;
-			System.out.println("5 - fin") ;
+			System.out.println("4 - fin") ;
 			System.out.print("Veuillez entrer votre choix : ");
 	
 			switch(choice) {
 			case 1 :
-				// Résoud manuellement le probleme en faisant appel é la méthode resoudManuelle
-				resoudManuelle(agg);//Régler le probleme de la variable agg
+				// Résoud manuellement le probleme en faisant appel à la méthode resoudManuelle qui prend comme paramètre la variable agg
+				resoudManuelle(agg);
 	
-				//je ne sais plus faire l'appel de la méthode pour moi c'etait resoudManuelle(agg); (basique mais je bloque)
 				break ;
 			case 2 :
-				// Résoud automatiquement le probleme en faisant appel é la méthode resoudAuto
-				MenuAlgorithmes.resoudAuto(agg);
+				// Résoud automatiquement le probleme en faisant appel à la méthode resoudAuto
+				MenuAlgorithmes.menuPrincipalAlgorithmes(agg);
 	
 				break ;
 			case 3 : 
 				//Sauvegarde le fichier en utilisant la méthode ecritureVersFichier de la classe LectureEcriture.
-				System.out.println("veuillez entrer le chemin oé sauvegarder le fichier");
+				System.out.println("veuillez entrer le chemin où sauvegarder le fichier");
 				LectureEcriture.ecritureVersFichier(sc.nextLine(), agg);
 				break;
 			case 4 : 
-				//Complexité des algorithmes 
-				break;
-			case 5 : 
 	
 				// Fermeture du programme
 				System.out.println("Fin du programme.");
 				sc.close();
 				break ;
 			default :
+				//affiche "choix incorrect" en cas de mauvais choix
 				System.out.println("Choix incorrect.");
 				break ;
 			}
-			exit = (choice != 5) ;
+			exit = (choice != 4) ;
 			sc.close();
 		}while (!exit);
 	}
 	
-//termine la boucle quand l'utilisateur choisit de fermer le programme (choix 5).
+//termine la boucle quand l'utilisateur choisit de fermer le programme (choix 4).
 	
-	public static Agglomeration aggloManuelle(){// je ne sais pas quel type de retour il faut utiliser 
+	
+	
+	//Méthode
+	public static Agglomeration aggloManuelle(){
 		Scanner sc = new Scanner(System.in);
 	
 		int nbVilles = 0 ;
 	
-		while(nbVilles < 1  || nbVilles > 26) { //Tant que le nombre n'est pas compris entre 1 et 26, on demande a l'utilisateur de saisir un nombre.
-			//TODO faire en sorte que la contrainte entre 1 et 26 saute
+		
 			System.out.print("Entrez un nombre de villes ");
 			try {
 				nbVilles = sc.nextInt(); 
@@ -112,15 +113,17 @@ public class MenuInitial {
 				System.err.println("Il faut rentrer un int.") ;
 				sc.next() ;
 				nbVilles = 0 ;
-			}
-		}
+			}// Demande à l'utilisateur de rentrer un nombre de ville qu'on va attribuer à la variable nbvilles
+		
 	
+			//Crée une agglomération en prenant comme paramètre nbVilles
 		Agglomeration agg = new Agglomeration(nbVilles);
 		System.out.println("Creee : "+agg.toString()+"\nLes ecoles sont dans les villes suivantes : ");
 		agg.afficheVilleAEcole() ;
 		int choice = 0; 
 		boolean exit ;
 	
+		//Crée un menu qui permet à l'utilisateur de créer son agglomération entièrement
 		do {
 			System.out.println("\n  - Menu 1 - ") ;
 			System.out.println("1 - ajouter une route");
@@ -136,7 +139,7 @@ public class MenuInitial {
 			}
 			switch(choice) {
 			case 1 :
-				//A MODIFIER tant que "Ville a" est null, demander a nouveau une ville
+				//Permet à l'utilisateur d'ajouter une route entre 2 villes en lui demandant de relier deux villes via leurs clés. Affiche ensuite la liste des routes de l'agglomération.
 				try {
 					System.out.print("\nEntrez la cle de la premiere ville a relier : ");
 					String villeA = sc.nextLine() ; //retourne une ville
@@ -151,9 +154,12 @@ public class MenuInitial {
 				}
 				break ;
 			case 2 :
+				
+				//Vérifie que l'agglomération est bien connexe. Le cas échéant, on demande à l'utilisateur de continuer à ajouter des routes
 				System.out.println("Est-ce que toutes les villes sont bien accessibles...\n"+(agg.estConnexe()?"Oui !\n":"Non ! Continuez d'ajouter des routes")) ;
 				break ;
 			case 3 :
+				//quitte le menu
 				System.out.println("Fin du programme.") ;
 				sc.close();
 				System.exit(0) ;
@@ -165,30 +171,17 @@ public class MenuInitial {
 			exit = (choice == 2) && agg.estConnexe() ;
 	
 		} while (!exit) ;
-		return agg;//tant que la ville n'est pas connexe et que l'utilisateur ne veut pas sortir
-	
 		
-		/* On cree ici le 1er menu qui va nous afficher les choix suivants : 
-		 * 1/ ajouter une route
-		 * 2/ j'ai rentre toutes mes routes" 
-		 * 3/ fin
-		
-				   La boucle while permet de verifier que l'agglomeration est connexe, c'est-a-dire qu'elle verifie que toute les villes sont reliees par au minimum une route.
-				   Tant que ce n'est pas le cas on continue de  demander a l'utilisateur d'ajouter des routes.
-		
-				   Si on choisit ajouter une route, on utilise la fonction void ajouterRoute en prenant en argument les deux villes rentrees au clavier.
-		
-				   Si on choisit 2, une verification est faite pour s'assurer que toutes villes sont reliees.
-				   Si ce n'est pas le cas, on affiche un message d'erreur a l'utilisateur afin qu'il continue. 
-		 */
 
 		
 		System.out.println("Fin de la premiere etape. Bilan :\n") ;
-		System.out.println(agg.toString()+"Les routes sont les suivantes \n") ;//regler la fonction afficheroutes
+		System.out.println(agg.toString()+"Les routes sont les suivantes \n") ;//affiche les routes à la fin de la première étape
 		agg.afficheRoutes() ;
+		return agg;
 	}
 	
-	private Agglomeration resoudManuelle(agg) {//pareil je ne sais pas quel type utiliser et si l'argument est bon
+	//Méthode qui permet de résoudre manuellement le problème en prenant en paramètre l'agglomération agg via un menu a 3 choix.
+	private void resoudManuelle(Agglomeration agg) {
 		int choice = 0;
 		Scanner sc = new Scanner(System.in) ;
 		while (choice != 3){ 
@@ -208,6 +201,8 @@ public class MenuInitial {
 	
 			switch(choice) {
 			case 1 :
+				
+				//Permet d'ajouter une école . Il n'est posssible d'en ajouter une que si il n'y a pas déjà une école dans la ville en question.
 				System.out.println("Entrez la cle de la ville dans laquelle vous souhaitez ajouter une ecole" );
 				try {
 					agg.ajouterEcole(sc.nextLine());
@@ -217,6 +212,8 @@ public class MenuInitial {
 				break ;
 	
 			case 2:
+				
+				//Permet de retirer une école . Il n'est possbile de la retirer que si la ville possède une école.
 				System.out.println("Entrez la cle de la ville dans laquelle vous souhaitez retirer une ecole : " );
 				try {
 					agg.retirerEcole(sc.nextLine());
@@ -226,6 +223,7 @@ public class MenuInitial {
 				break ;
 	
 			case 3:
+				System.out.println("Fin");
 				
 				break ;
 	
@@ -234,7 +232,7 @@ public class MenuInitial {
 				break ;
 			}
 			System.out.println("Les ecoles sont dans les villes suivantes : ") ;
-			agg.afficheVilleAEcole();
+			agg.afficheVilleAEcole();//Affiche les villes qui possèdent des écoles.
 		}
 		System.out.println("\nBilan : \n"+agg.toString()+"\nLes ecoles sont dans les villes suivantes : ") ;
 		agg.afficheVilleAEcole();
---
+++ src/main/menus/MenuPrincipal.java
@@ -0,0 +1,5 @@
+package main.menus;
+
+public class MenuPrincipal {
+
+}
